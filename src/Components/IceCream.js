0da2e8617c4fdb161146449d51363fac670b030c
Fred Collman <fredcollman@gmail.com>
2021-02-28 16:36:35 +0000

    Implement ice cream

--- src/Components/IceCream.js
+++ src/Components/IceCream.js
@@ -1,3 +1,12 @@
-const IceCream = () => "yum";
+import "./IceCream.css";
+
+const IceCream = () => (
+  <svg class="vanilla ice-cream" viewBox="0 0 100 120">
+    <g>
+      <path class="cone" d="M 20 30 l 30 90 l 30 -90 z" />
+      <circle class="scoop" cx="50" cy="20" r="33" />
+    </g>
+  </svg>
+);
 
 export default IceCream;
