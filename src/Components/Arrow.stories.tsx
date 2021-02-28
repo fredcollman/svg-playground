97fe32b67b1cd39d0217d915887946015e0a3c33
Fred Collman <fredcollman@gmail.com>
2021-02-28 17:43:06 +0000

    Add an arrow pointing to the right

---
+++ src/Components/Arrow.stories.tsx
@@ -0,0 +1,11 @@
+import { Story } from "@storybook/react";
+import Arrow from "./Arrow";
+
+const config = {
+  title: "Arrow",
+  component: Arrow,
+};
+
+export default config;
+
+export const Default = Arrow.bind({});
---
+++ src/Components/Arrow.tsx
@@ -0,0 +1,9 @@
+import "./Arrow.css";
+
+const Arrow = () => (
+  <svg className={`arrow`} viewBox="0 0 10 10">
+    <path d="M 1 5 h 8 m -4 -4 l 4 4 l -4 4" />
+  </svg>
+);
+
+export default Arrow;
